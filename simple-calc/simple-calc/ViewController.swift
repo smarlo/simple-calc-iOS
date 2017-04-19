caf6b398bec2f74014bd9bc950deba6cb7cfb6e9
Sabrina Niklaus <sabrinamarlo@D-173-250-202-143.dhcp4.washington.edu>
2017-04-18 18:53:15 -0700

    enabled more than two arguments for basic operations

--- simple-calc/simple-calc/ViewController.swift
+++ simple-calc/simple-calc/ViewController.swift
@@ -12,11 +12,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var calcDisplay: UILabel!
     public final let possibleOperands: [String] = ["+", "-", "/", "%", "x", "count", "avg", "fact"]
     
-    public var currentOperator: ((Int, Int) -> Int)? = nil
+    public var numPressed: Bool = true
+    public var avgPressed: Bool = false
+    public var countPressed: Bool = false
+    public var factPressed: Bool = false
     
     @IBAction func numberPressed(_ sender: Any) {
-        let number = (sender as AnyObject).titleLabel!!.text
-        addNum(number!)
+        if !factPressed {
+            let number = (sender as AnyObject).titleLabel!!.text
+            addNum(number!)
+            numPressed = true
+        }
     }
     
     func addNum(_ num: String) {
@@ -28,68 +34,65 @@ class ViewController: UIViewController {
     }
     
     @IBAction func add(_ sender: Any) {
-        currentOperator = add
-        calcDisplay.text! += " + "
+        if numPressed {
+            calcDisplay.text! += " + "
+        }
     }
     
     @IBAction func subtract(_ sender: Any) {
-        currentOperator = subtract
-        calcDisplay.text! += " - "
+        if numPressed {
+            calcDisplay.text! += " - "
+        }
     }
     
     @IBAction func multiply(_ sender: Any) {
-        currentOperator = multiply
-        calcDisplay.text! += " x "
+        if numPressed {
+            calcDisplay.text! += " x "
+        }
     }
     
     @IBAction func divide(_ sender: Any) {
-        currentOperator = divide
-        calcDisplay.text! += " / "
+        if numPressed {
+            calcDisplay.text! += " / "
+        }
     }
     
     @IBAction func modulo(_ sender: Any) {
-        currentOperator = modulo
-        calcDisplay.text! += " % "
+        if numPressed {
+            calcDisplay.text! += " % "
+        }
     }
     
     @IBAction func count(_ sender: Any) {
-        calcDisplay.text! += " count "
+        if numPressed && !avgPressed && !factPressed {
+            calcDisplay.text! += " count "
+            countPressed = true
+            numPressed = false
+        }
     }
     
     @IBAction func average(_ sender: Any) {
-        calcDisplay.text! += " avg "
+        if numPressed && !factPressed && !countPressed {
+            calcDisplay.text! += " avg "
+            avgPressed = true
+            numPressed = false
+        }
     }
     
     @IBAction func factorial(_ sender: Any) {
-        calcDisplay.text! += " fact "
+        if numPressed && !countPressed && !avgPressed {
+            calcDisplay.text! += " fact "
+            factPressed = true
+            numPressed = false
+        }
     }
     
     @IBAction func clear(_ sender: Any) {
         calcDisplay.text! = "0"
-    }
-    
-    func operate(left: Int, right: Int, operation: (Int, Int) -> Int) -> Int {
-        return operation(left, right)
-    }
-    
-    func add(left: Int, right: Int) -> Int {
-        return left + right
-    }
-    
-    func subtract(left: Int, right: Int) -> Int {
-        return left - right
-    }
-    
-    func multiply(left: Int, right: Int) -> Int {
-        return left * right
-    }
-    
-    func divide(left: Int, right: Int) -> Int {
-        return left / right
-    }
-    
-    func modulo(left: Int, right: Int) -> Int {
-        return left % right
+        factPressed = false
+        avgPressed = false
+        countPressed = false
+        numPressed = false
     }
     
     func count(nums: [Int]) -> Int {
@@ -116,30 +119,52 @@ class ViewController: UIViewController {
     
     @IBAction func calculate(_ sender: Any) {
         let arg = calcDisplay.text!
-        
-        if arg.contains("count") || arg.contains("avg") || arg.contains("fact") {
-            let numString: [String] = arg.characters.split(separator: " ").map(String.init)
-            var numbers: [Int] = []
-            for i in 0...numString.count - 1 {
-                if !possibleOperands.contains(numString[i]) {
-                    numbers.append(Int(numString[i])!)
-                }
+        let numString: [String] = arg.characters.split(separator: " ").map(String.init)
+        var numbers: [Int] = []
+        var operators: [String] = []
+        for i in 0...numString.count - 1 {
+            if !possibleOperands.contains(numString[i]) {
+                numbers.append(Int(numString[i])!)
+            } else if possibleOperands.contains(numString[i]) {
+                operators.append(numString[i])
             }
+        }
+
+        if arg.contains("count") || arg.contains("avg") || arg.contains("fact") {
             
             if arg.contains("count") {
                 calcDisplay.text = String(count(nums: numbers))
+                countPressed = false
             } else if arg.contains("avg") {
                 calcDisplay.text = String(average(nums: numbers))
+                avgPressed = false
             } else if arg.contains("fact") {
                 calcDisplay.text = String(factorial(num: numbers[0]))
+                factPressed = false
             }
+            
         } else if arg.contains("+") || arg.contains("-") || arg.contains("/") || arg.contains("%") || arg.contains("x") {
-            let numbers = arg.characters.split(separator: " ")
-            let left: Int = Int(String(numbers[0]))!
-            let right: Int = Int(String(numbers[2]))!
-            calcDisplay.text! = String(operate(left: left, right: right, operation: currentOperator!))
+            var result = numbers[0]
+            for i in 0...operators.count - 1 {
+                result = processArgs(left: result, right: numbers[i + 1], op: operators[i])
+            }
+            calcDisplay.text! = String(result)
         }
+        numPressed = true
         
+    }
+    
+    func processArgs(left: Int, right: Int, op: String) -> Int {
+        var total: Int = left
+        switch op {
+            case "+" : total += right
+            case "-" : total -= right
+            case "x" : total *= right
+            case "/" : total /= right
+            case "%" : total = total % right
+            default : total += 0
+        }
+        return total
     }
     
     override func viewDidLoad() {
